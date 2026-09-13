326bb427e43d83bcfb63cdebe67377adcfa5372c
sergijminda9 <sergijminda9@gmail.com>
2026-09-13 17:06:13 +0300

    fix: explicitly pass SMTP_FROM as sender in requestResetEmail

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -138,6 +138,7 @@ export const requestResetEmail = async (req, res, next) => {
 
   try {
     await sendEmail({
+      from: process.env.SMTP_FROM,
       to: email,
       subject: 'Reset your password',
       html,
